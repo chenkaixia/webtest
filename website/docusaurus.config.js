7b3e12584de4ccf6866e283c218c31c1404a1e3e
大海 <chenkaixia@cai-inc.com>
2022-03-07 13:52:26 +0800

    doc:设置文件个格式

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,16 +6,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'IData Pro',
-  tagline: 'IData are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'webtest',
+  tagline: 'webtest are cool',
+  url: 'https://chenkaixia.github.io/webtest/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'IData', // Usually your GitHub org/user name.
-  projectName: 'IData', // Usually your repo name.
-
+  organizationName: 'chenkaixia',
+  projectName: 'webtest',
+  deploymentBranch:"master",
+  trailingSlash:false,
   presets: [
     [
       'classic',
@@ -23,16 +24,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -46,70 +38,11 @@ const config = {
       navbar: {
         // title: 'IData Pro',
         logo: {
-          alt: 'My Site Logo',
-          // src: 'img/logo.svg',
+          alt: 'IDATA',
           src: 'https://sitecdn.zcycdn.com/f2e-assets/b609cee4-c7d7-45bf-86da-3f5263fb8fa1.svg'
         },
-        items: [
-          // {
-          //   type: 'doc',
-          //   docId: 'intro',
-          //   position: 'right',
-          //   label: '文档',
-          // },
-          // {
-          //   href: 'https://git.cai-inc.com/bigdata/server/idata',
-          //   label: 'GitLab',
-          //   position: 'right',
-          // },
-        ],
-      },
-      // footer: {
-      //   style: 'dark',
-      //   links: [
-      //     {
-      //       title: 'Docs',
-      //       items: [
-      //         {
-      //           l: 'Tutorial',
-      //           to: '/docs/intro',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'Community',
-      //       items: [
-      //         {
-      //           label: 'Stack Overflow',
-      //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-      //         },
-      //         {
-      //           label: 'Discord',
-      //           href: 'https://discordapp.com/invite/docusaurus',
-      //         },
-      //         {
-      //           label: 'Twitter',
-      //           href: 'https://twitter.com/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'More',
-      //       items: [
-      //         {
-      //           label: 'Blog',
-      //           to: '/blog',
-      //         },
-      //         {
-      //           label: 'GitHub',
-      //           href: 'https://github.com/facebook/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //   ],
-      //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      // },
-      prism: {
+        items: [],
+      },  prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
