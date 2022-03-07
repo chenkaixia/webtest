37d1d369a117fc7f8112171e165a10c11c4f98ce
大海 <chenkaixia@cai-inc.com>
2022-03-07 16:39:11 +0800

    修改配置

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,16 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'webtest',
-  tagline: 'webtest are cool',
+  title: 'idata',
+  favicon: '/img/favicon.ico',
   url: 'https://chenkaixia.github.io',
+  tagline:  'IData是一个现代化的大数据开发管理平台，提供完整的大数据开发流程，关注并聚焦于数据的全链路生命周期，便捷高效的满足大数据开发需求。',
   baseUrl: '/webtest/',
   projectName: 'webtest',
   organizationName: 'chenkaixia',
-  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
   deploymentBranch:"deployment",
   presets: [
     [
